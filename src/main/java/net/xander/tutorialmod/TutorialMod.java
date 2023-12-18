b482aa2b615e0523ad752fcabe11524009d18a29
Xander Brown <xanderbrown05@gmail.com>
2023-12-18 17:34:52 -0400

    added an important comment

--- src/main/java/net/xander/tutorialmod/TutorialMod.java
+++ src/main/java/net/xander/tutorialmod/TutorialMod.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
+//very important comment
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod
