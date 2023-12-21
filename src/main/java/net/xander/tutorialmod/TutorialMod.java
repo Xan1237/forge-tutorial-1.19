f791131532542e350c9d0196cbe612330741d2da
Xander Brown <xanderbrown05@gmail.com>
2023-12-20 23:32:42 -0400

    added new blocks

--- src/main/java/net/xander/tutorialmod/TutorialMod.java
+++ src/main/java/net/xander/tutorialmod/TutorialMod.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.xander.tutorialmod.block.ModBlocks;
 import net.xander.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 //very important comment
@@ -24,7 +25,7 @@ public class TutorialMod
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
-
+        ModBlocks.register(modEventBus);
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
